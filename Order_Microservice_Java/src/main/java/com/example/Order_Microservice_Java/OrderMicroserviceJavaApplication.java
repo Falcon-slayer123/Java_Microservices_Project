ddd6d1319aae46331b57b40cb32daf511aa143fe
Falcon-slayer123 <himanshutrivedi133@gmail.com>
2024-12-08 18:22:56 +0530

    Order Microservice

---
+++ Order_Microservice_Java/src/main/java/com/example/Order_Microservice_Java/OrderMicroserviceJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Order_Microservice_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMicroserviceJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderMicroserviceJavaApplication.class, args);
+	}
+
+}
---
+++ Order_Microservice_Java/src/test/java/com/example/Order_Microservice_Java/OrderMicroserviceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Order_Microservice_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMicroserviceJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
