2fa8d1d61456cf2b98cdc3b4ca2d99c43bc1c7a4
Falcon-slayer123 <himanshutrivedi133@gmail.com>
2024-12-11 21:31:40 +0530

    Added WebClientConfig in Order Service implementation

--- Order_Microservice_Java/src/main/java/com/example/Order_Microservice_Java/Config/WebClientConfig.java
+++ Order_Microservice_Java/src/main/java/com/example/Order_Microservice_Java/Config/WebClientConfig.java
@@ -1,9 +1,16 @@
 package com.example.Order_Microservice_Java.Config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder().build();
+    }
 
 }
--- Order_Microservice_Java/src/main/java/com/example/Order_Microservice_Java/Service/OrderService.java
+++ Order_Microservice_Java/src/main/java/com/example/Order_Microservice_Java/Service/OrderService.java
@@ -6,35 +6,43 @@ import com.example.Order_Microservice_Java.DTO.OrderedRequest;
 import com.example.Order_Microservice_Java.Model.Order;
 import com.example.Order_Microservice_Java.Model.OrderLineItems;
 import com.example.Order_Microservice_Java.Repository.OrderRepository;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
+
+
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, WebClient webClient) {
         this.orderRepository = orderRepository;
+        this.webClient = webClient;
     }
 
-    public void placeOrder(OrderedRequest orderedRequest)
-    {
+    public void placeOrder(OrderedRequest orderedRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
-      List<OrderLineItems> orderLineItems= orderedRequest.getOrderLineItemsdtoList().stream().map(this::mapToDto).toList();
-     order.setOrderLineItems(orderLineItems);
-     //Calling Inventory Service If Product Available in Stock//
+        List<OrderLineItems> orderLineItems = orderedRequest.getOrderLineItemsdtoList().stream().map(this::mapToDto).toList();
+        order.setOrderLineItems(orderLineItems);
+        //Calling Inventory Service If Product Available in Stock//
+        Boolean result = webClient.get().uri("http://localhost:8082/api.inventory").retrieve().bodyToFlux(boolean.class).blockFirst();
 
-     orderRepository.save(order);
+        if (result) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product not in stock");
+        }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsdto orderLineItemsdto) {
 
-        OrderLineItems orderLineItems= new OrderLineItems();
+        OrderLineItems orderLineItems = new OrderLineItems();
         orderLineItems.setPrice(orderLineItemsdto.getPrice());
         orderLineItems.setQuantity(orderLineItemsdto.getQuantity());
         orderLineItems.setSkuCode(orderLineItems.getSkuCode());
